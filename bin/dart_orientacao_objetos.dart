ff1d0d2ce315740cb2c6713e52d7f5da1eda29a8
Otavio Henrique de Lima e Silva <otaviohlsilva@gmail.com>
2022-12-19 12:37:03 -0300

    commit 1

---
+++ bin/dart_orientacao_objetos.dart
@@ -0,0 +1,18 @@
+void main(List<String> arguments) {
+  // print("Ola mundo!");
+  String nome = 'Laranja';
+  double peso = 100.2;
+  String cor = "Verde e Amarela";
+  String sabor = "Doce e cítrica";
+  int diasDesdeColheita = 40;
+  bool isMadura = funcEstaMadura(diasDesdeColheita);
+  print(isMadura);
+}
+
+bool funcEstaMadura(int dias) {
+  if (dias >= 30) {
+    return true;
+  } else {
+    return false;
+  }
+}
---
+++ test/dart_orientacao_objetos_test.dart
@@ -0,0 +1,8 @@
+import 'package:dart_orientacao_objetos/dart_orientacao_objetos.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate', () {
+    expect(calculate(), 42);
+  });
+}
